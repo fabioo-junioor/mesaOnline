e82a706532883bf975a316b5782bae673e5e5ce1
fabioo-junioor <fj_011@hotmail.com>
2023-11-28 21:13:15 -0300

    initial project configuration

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="app">
+    <h3>Em desenvolvimento</h3>
+  </div>
+</template>
+
+<style scoped>
+#app{
+  background-color: grey;
+  
+}
+
+</style>
