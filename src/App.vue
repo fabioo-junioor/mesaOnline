8ed3115d63c80c1bff0deea8838f4b35890be2bc
Fábio Júnior <fj_011@hotmail.com>
2023-11-29 21:50:14 -0300

    add primevue, sass and sass-loader

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,63 @@
 <script setup>
+import { ref } from 'vue'
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home'
+    },
+    {
+        label: 'Features',
+        icon: 'pi pi-star'
+    },
+    {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+            {
+                label: 'Components',
+                icon: 'pi pi-bolt'
+            },
+            {
+                label: 'Blocks',
+                icon: 'pi pi-server'
+            },
+            {
+                label: 'UI Kit',
+                icon: 'pi pi-pencil'
+            },
+            {
+                label: 'Templates',
+                icon: 'pi pi-palette',
+                items: [
+                    {
+                        label: 'Apollo',
+                        icon: 'pi pi-palette'
+                    },
+                    {
+                        label: 'Ultima',
+                        icon: 'pi pi-palette'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Contact',
+        icon: 'pi pi-envelope'
+    }
+]);
 
 </script>
 
 <template>
   <div id="app">
-    <h3>Em desenvolvimento...</h3>
+    <h3>Em desenvolvimento</h3>
+    <Button label="Submit" icon="pi pi-check" />
+    <Menubar :model="items" />
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 #app{
   background-color: grey;
   
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* primevue */
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import Menubar from 'primevue/menubar';
+
+
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import 'primeicons/primeicons.css'
+
+
+createApp(App)
+    .use(PrimeVue)
+    .component('Button', Button)
+    .component('Menubar', Menubar)
+    .mount('#app')
